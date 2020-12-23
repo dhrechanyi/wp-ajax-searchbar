bf2587945b0e18b7888996847d48e8b0c0af70f3
Denys Hrechanyi <dhrechanyi@gmail.com>
2020-12-23 16:15:42 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+   /*
+   Plugin Name: WP Ajax Searchbar
+   Plugin URI: 
+   Description: Adds ajax seaarchbar shortcode 
+   Version: 1.0
+   Author: Denys Hrechanyi
+   Author URI: https://hrechanyi.pro/contact-me
+   License: GPL2
+   */
+
+?>
